0f7270038a6b3d66782a9ae5c8999eaef8490005
oxica <kskaplun@gmail.com>
2022-01-11 22:04:25 +0200

    add task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,1 +1,19 @@
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+
+let ingredientEl;
+
+for (let ingredient of ingredients) {
+  ingredientEl = document.createElement("li");
+  ingredientEl.textContent = [ingredient];
+  ingredientEl.classList.add('class="item"');
+
+  document.getElementById("ingredients").appendChild(ingredientEl);
+}
 
